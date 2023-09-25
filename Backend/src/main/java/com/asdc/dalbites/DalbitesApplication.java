b1aa910102a9fa35baeb756d428ae020dca8fef5
ashish nagpal <as889330@dal.ca>
2023-09-25 00:56:23 -0300

    :tada: Spring Initializer

---
+++ Backend/src/main/java/com/asdc/dalbites/DalbitesApplication.java
@@ -0,0 +1,13 @@
+package com.asdc.dalbites;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DalbitesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DalbitesApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/asdc/dalbites/DalbitesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asdc.dalbites;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DalbitesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
