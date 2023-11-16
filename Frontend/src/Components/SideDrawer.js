52f193412b85f2621ac74b3690e8d04ff687d2f8
ashish nagpal <as889330@dal.ca>
2023-11-16 01:10:19 -0400

    drawer-addition

--- Frontend/src/Components/SideDrawer.js
+++ Frontend/src/Components/SideDrawer.js
@@ -3,14 +3,24 @@ import { createDrawerNavigator } from "@react-navigation/drawer";
 import Profile from "../../screens/Profile";
 import CustomLogout from "../../screens/CustomLogout";
 import HomeTabGroup from "./HomeTabGroup";
+import Review from "../../screens/Review";
 
 const Drawer = createDrawerNavigator();
 
 const SideDrawer = () => {
   return (
     <Drawer.Navigator screenOptions={{ headerTitle: "" }}>
-      <Drawer.Screen name="HomeStackGroup" component={HomeTabGroup} />
+      <Drawer.Screen
+        name="HomeStackGroup"
+        options={{ title: "Home" }}
+        component={HomeTabGroup}
+      />
       <Drawer.Screen name="Profile" component={Profile} />
+      <Drawer.Screen
+        name="Review"
+        component={Review}
+        options={{ drawerLabel: () => null, drawerIcon: () => null }}
+      />
       <Drawer.Screen name="Logout" component={CustomLogout} />
     </Drawer.Navigator>
   );
