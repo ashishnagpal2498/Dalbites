01ba4d89dd508d7e7e2bb227a818b29504196633
ashish nagpal <as889330@dal.ca>
2023-11-18 01:36:19 -0400

    tdd | create review test

--- Backend/src/test/java/com/asdc/dalbites/service/impl/ReviewServiceImplTest.java
+++ Backend/src/test/java/com/asdc/dalbites/service/impl/ReviewServiceImplTest.java
@@ -1,12 +1,41 @@
 package com.asdc.dalbites.service.impl;
 
+import com.asdc.dalbites.model.DAO.RestaurantDao;
+import com.asdc.dalbites.model.DAO.UserDao;
+import com.asdc.dalbites.model.DTO.ReviewDTO;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+
+import org.mockito.MockitoAnnotations;
 
 public class ReviewServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @InjectMocks
+    private ReviewServiceImpl reviewService;
 
     @Test
     public void testCreateReview(){
         // Submit a review
+        ReviewDTO reviewDTO = new ReviewDTO();
+        reviewDTO.setRating(4);
+        reviewDTO.setReviewComment("Great experience");
+        reviewDTO.setRestaurantId(1L);
+        reviewDTO.setUserId(2L);
+
+        RestaurantDao mockedRestaurant = new RestaurantDao();
+        mockedRestaurant.setId(1L);
+
+        UserDao mockedUser = new UserDao();
+        mockedUser.setUserId(2L);
+
+        reviewService.createReview();
     }
 
     @Test
