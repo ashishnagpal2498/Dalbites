3f8baa8ebe8e9ecb03a268a736417dbb9186ce1a
ashish nagpal <as889330@dal.ca>
2023-11-18 01:06:58 -0400

    tdd | empty service class

---
+++ Backend/src/main/java/com/asdc/dalbites/service/ReviewService.java
@@ -0,0 +1,7 @@
+package com.asdc.dalbites.service;
+
+public interface ReviewService {
+    void createReview();
+    void getAllRestaurantReviews();
+    void getAllUserReviews();
+}
---
+++ Backend/src/main/java/com/asdc/dalbites/service/impl/ReviewServiceImpl.java
@@ -0,0 +1,22 @@
+package com.asdc.dalbites.service.impl;
+
+import com.asdc.dalbites.service.ReviewService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ReviewServiceImpl implements ReviewService {
+    @Override
+    public void createReview(){
+
+    }
+
+    @Override
+    public void getAllRestaurantReviews(){
+
+    }
+
+    @Override
+    public void getAllUserReviews(){
+
+    }
+}
