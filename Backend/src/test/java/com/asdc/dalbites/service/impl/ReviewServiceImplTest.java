e13d777a171c223bf5b6befe0cd4636717a2d7d4
ashish nagpal <as889330@dal.ca>
2023-11-18 00:59:47 -0400

    tdd implementation

---
+++ Backend/src/test/java/com/asdc/dalbites/service/impl/ReviewServiceImplTest.java
@@ -0,0 +1,21 @@
+package com.asdc.dalbites.service.impl;
+
+import org.junit.jupiter.api.Test;
+
+public class ReviewServiceImplTest {
+
+    @Test
+    public void testCreateReview(){
+        // Submit a review
+    }
+
+    @Test
+    public void testGetAllRestaurantReviews(){
+        // Get all reviews of a restaurant
+    }
+
+    @Test
+    public void testGetAllUserReviews(){
+        // Get all reviews of a user
+    }
+}
