9805e97c5ec3761b5cef845e3365caf70c8d163f
ashish nagpal <as889330@dal.ca>
2023-11-18 02:44:40 -0400

    tdd | controller

--- Backend/src/test/java/com/asdc/dalbites/service/impl/ReviewServiceImplTest.java
+++ Backend/src/test/java/com/asdc/dalbites/service/impl/ReviewServiceImplTest.java
@@ -15,14 +15,12 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.*;
