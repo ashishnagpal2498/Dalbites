682bd4d1e9c4b72cce0452349e1015883a970784
Shubham Vijaykumar Patel <sh659429@dal.ca>
2023-11-26 09:14:59 -0400

    fix: implementation smells

--- Backend/src/main/java/com/asdc/dalbites/util/Constants.java
+++ Backend/src/main/java/com/asdc/dalbites/util/Constants.java
@@ -5,4 +5,8 @@ public class Constants {
     public static final Integer RANDOM_NUMBER_START = 100000;
     public static final Integer RANDOM_NUMBER_END = 900000;
     public static final Integer TEMP_TOKEN_EXPIRY_TIME = 15;
+    public static final Long PAYMENT_SERVICE_AMOUNT_CONSTANT = 1099L;
+    public static final String STRIPE_API_KEY = "sk_test_51OE1QOAxY201a6THmRb6QUUvbQGe7Ec36W0jK6wrHlis3nUHLgquwHGslPI3aDrwpblmugDy49EpukU5dmyHzG66002ribyeu9";
+    public static final String ORDER_READY_BODY = "Dear Customer,\n\nYour order is now ready for pickup. Please visit the restaurant to collect your order.";
+    public static final String ORDER_CONFIRMATION_BODY = "Thank you for placing your order with Dalbites. Here are the details of your order:\n\n";
 }
