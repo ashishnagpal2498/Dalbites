1da4a4725568202f9f2c8ec1f7537d8cfd7540aa
Shubham Bhushan Pawar <sh764760@dal.ca>
2023-11-26 14:01:40 -0400

    refactor: restaurant + user| view

--- Frontend/src/Components/CartItem.js
+++ Frontend/src/Components/CartItem.js
@@ -33,28 +33,38 @@ const CartItem = ({cardData}) => {
 
   return (
     <View style={styles.card}>
+
       <View style={styles.itemHolder}>
-        <Image
-          src={cardData.menu_image}
-          style={styles.menuItemImage}
-        />
+        <View style={styles.itemImageContainer}>
+          <Image
+            src={cardData.menu_image}
+            style={styles.menuItemImage} 
+          />
+        </View>
         <View style={styles.itemDetailsHolder}>
-          <Text style={styles.title}>{cardData.name}</Text>
-          <Text style={styles.price}>Price: ${cardData.price}</Text>
-          <View style={styles.quantityContainer}>
-            <TouchableOpacity style={tw`bg-yellow-500 rounded-lg py-2 px-4`} onPress={decreaseQuantity}>
-              <Text style={tw`text-black text-lg font-semibold`}>-</Text>
-            </TouchableOpacity>
-            <Text style={styles.quantity}>{quantity}</Text>
-            <TouchableOpacity style={tw`bg-yellow-500 rounded-lg py-2 px-4`} onPress={increaseQuantity}>
-              <Text style={tw`text-black text-lg font-semibold`}>+</Text>
-            </TouchableOpacity>
-          </View>
+          <Text style={styles.itemName}>{cardData.name}</Text>
+          <Text style={styles.orderCardText}>Cost: ${" "}{cardData.price}</Text>
+          <Text style={styles.orderCardText}>
+          Total Cost: ${" "}{Number(cardData.price * cardData.quantity).toPrecision(4)}
+          </Text>
         </View>
       </View>
-      <TouchableOpacity style={tw`bg-yellow-500 py-2 px-4`} onPress={removeFromCart}>
-        <Text style={tw`text-black text-lg font-semibold text-center justify-center content-center`}>Remove from Cart</Text>
-      </TouchableOpacity>
+
+      <View style={styles.buttonsContainer}>
+        <TouchableOpacity style={styles.removeButton} onPress={removeFromCart}>
+          <Text style={styles.removeButtonText}>Remove</Text>
+        </TouchableOpacity>
+        <View style={styles.quantityContainer}>
+          <TouchableOpacity style={tw`bg-yellow-500 rounded-lg py-2 px-4`} onPress={decreaseQuantity}>
+            <Text style={styles.ButtonText}>-</Text>
+          </TouchableOpacity>
+          <Text style={styles.quantity}>{quantity}</Text>
+          <TouchableOpacity style={tw`bg-yellow-500 rounded-lg py-2 px-4`} onPress={increaseQuantity}>
+            <Text style={styles.ButtonText}>+</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+
     </View>
   );
 };
@@ -75,23 +85,13 @@ const styles = StyleSheet.create({
       width: '100%',
       resizeMode: 'cover',
     },
-    title: {
+    itemName: {
       fontSize: 20,
-      fontWeight: 'bold',
-      marginBottom: 8,
-      paddingHorizontal: 16,
-      paddingTop: 8,
+      fontWeight: "bold",
     },
-    description: {
-      fontSize: 16,
-      marginBottom: 8,
-      paddingHorizontal: 16,
-    },
-    price: {
-      fontSize: 18,
-      fontWeight: 'bold',
-      marginBottom: 8,
-      paddingHorizontal: 16,
+    orderCardText: {
+      fontSize: 15,
+      fontWeight: "500",
     },
     quantityContainer: {
       flexDirection: 'row',
@@ -103,17 +103,56 @@ const styles = StyleSheet.create({
       fontSize: 18,
       marginHorizontal: 8,
     },
-    menuItemImage: {
-      width: '50%',
-      maxHeight: 150
-    },
     itemHolder: {
-      display: 'flex',
-      flexDirection: 'row'
+      flexDirection: "row",
+      width: "95%",
+      alignSelf: "center",
+      marginVertical: 5,
+    },
+    itemImageContainer:{
+      flex: 1.2,
+      width: "100%",
+      aspectRatio: 1,
+      overflow: "hidden",
+      borderBottomLeftRadius: 10,
+      borderTopLeftRadius: 10,
+      padding: 5,
+    },
+    menuItemImage: {
+      width: "100%",
+      height: "100%",
+      borderRadius: 10,
     },
     itemDetailsHolder: {
-      width: '50%',
-    }
+      flex: 3,
+      justifyContent: "space-evenly",
+      padding: 5,
+    },
+    buttonsContainer:{
+      flexDirection:"row",
+      justifyContent:"space-between",
+    },
+    removeButton:{
+      margin: 7,
+      marginTop:0,
+      paddingRight:9,
+      borderRightWidth:2,
+      width:"50%",
+      alignSelf: "center",
+      alignItems: "center",
+    },
+    removeButtonText:{
+      backgroundColor: "#EAB308",
+      paddingVertical: 7,
+      paddingHorizontal: 15,
+      borderRadius: 10,
+      fontWeight:"bold",
+      fontSize: 21,
+    },
+    ButtonText:{
+      fontWeight:"bold",
+      fontSize: 21,
+    },
   });
 
 export default CartItem;
